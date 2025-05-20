7d5792d1ae38d5c161d330f497ef5713ceb75ea9
Xor01 <111096981+Xor01@users.noreply.github.com>
2025-05-20 11:09:36 +0300

    Update environment variables in template.env and configure SECRET_KEY in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,11 @@
 import os
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
 from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['UPLOAD_FOLDER'] = 'app/static/uploads/'
 
@@ -20,9 +18,5 @@ if not os.path.exists(app.config['UPLOAD_FOLDER'] + "images"):
 if not os.path.exists(app.config['UPLOAD_FOLDER']  + "voices"):
     os.makedirs(app.config['UPLOAD_FOLDER'] + "voices")
     
-# db = SQLAlchemy(app)
-# bcrypt = Bcrypt(app)
-# login_manager = LoginManager(app)
-# login_manager.login_view = 'login'
-load_dotenv()
+
 from app import routes
